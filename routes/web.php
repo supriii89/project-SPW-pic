069a20f3bd6ace47d905d80bada4062565c57cad
supriii89 <thebroklin789@gmail.com>
2026-04-24 13:02:50 +0700

    buat model dari produk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Produkcontroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('products', [Produkcontroller::class, 'index']);
+Route::post('products', [Produkcontroller::class, 'store']);
+Route::get('products/{id}', [Produkcontroller::class, 'show']);
+Route::put('products/{id}', [Produkcontroller::class, 'update']);
+Route::delete('products/{id}', [Produkcontroller::class, 'destroy']);
