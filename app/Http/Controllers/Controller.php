b761c846bf8910ebc1871c026ec5b531a2688f7f
MeivitaBungaCantika <bungkarnocantikkk@gmail.com>
2026-04-24 14:02:35 +0700

    update controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Routing\Controller;
+
+class LaporanController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            ['tanggal'=>'2026-06-01','total'=>29000,'untung'=>4000],
+            ['tanggal'=>'2026-06-02','total'=>50000,'untung'=>10000],
+            ['tanggal'=>'2026-06-03','total'=>20000,'untung'=>-2000],
+        ];
+
+        return view('laporan', compact('data'));
+    }
 }
